062fcab47465b11623a2b5223e81451ca93b0892
nakulshn <nakulshn@gmail.com>
2026-01-16 13:31:05 -0800

    Changed data

--- expriment/article/generate_data.R
+++ expriment/article/generate_data.R
@@ -30,43 +30,43 @@ n <- 4000
 p <- 800
 sigma_true <- 1.0
 
-# # ---------------------------
-# # Generate X -- iid N(0, 1/n) entries, seeds 1, 2, and 3
-# # ---------------------------
-# X <- matrix(rnorm(n * p, mean = 0, sd = sqrt(1 / n)), n, p)
-
-
-
 # ---------------------------
-# Generate X -- iid multivariate Gaussian rows, with correlated coordinates. seeds 4, 5, and 6
+# Generate X -- iid N(0, 1/n) entries
 # ---------------------------
-
-rho <- 0.7  # correlation level
-
-# Toeplitz covariance
-Sigma <- toeplitz(rho^(0:(p - 1)))
-
-# Generate iid rows
-X <- MASS::mvrnorm(
-  n = n,
-  mu = rep(0, p),
-  Sigma = Sigma
-) / sqrt(n)
+X <- matrix(rnorm(n * p, mean = 0, sd = sqrt(1 / n)), n, p)
 
 
-# ---------------------------
-# Generate betas: Basic Gaussian N(0, 1) truncated at [-3, 3]. Seed 1 and 4
-# ---------------------------
-
-beta <- rtruncnorm(
-  p,
-  a = -3, b = 3,
-  mean = 0,
-  sd = 1.0
-)
 
 # # ---------------------------
-# # Generate betas: truncated continuous spike-and-slab Seed 2 and 5
+# # Generate X -- iid multivariate Gaussian rows, with correlated coordinates.
+# # ---------------------------
+
+# rho <- 0.7  # correlation level
+
+# # Toeplitz covariance
+# Sigma <- toeplitz(rho^(0:(p - 1)))
+
+# # Generate iid rows
+# X <- MASS::mvrnorm(
+#   n = n,
+#   mu = rep(0, p),
+#   Sigma = Sigma
+# ) / sqrt(n)
+
+
+# # ---------------------------
+# # Generate betas: Basic Gaussian N(0, 1) truncated at [-3, 3].
+# # ---------------------------
+
+# beta <- rtruncnorm(
+#   p,
+#   a = -3, b = 3,
+#   mean = 0,
+#   sd = 1.0
+# )
+
+# # ---------------------------
+# # Generate betas: truncated continuous spike-and-slab Seeds 1, 2, 3
 # # ---------------------------
 # pi_slab <- 0.5
 # tau0 <- 0.04
@@ -90,29 +90,29 @@ beta <- rtruncnorm(
 # )
 
 
-# # ---------------------------
-# # Generate betas: bimodal Seed 3 and 6
-# # ---------------------------
-# pi_left_mode <- 0.5
-# tau0 <- 0.2
-# tau1 <- 0.2
+# ---------------------------
+# Generate betas: bimodal Seed 4,5,6
+# ---------------------------
+pi_left_mode <- 0.5
+tau0 <- 0.2
+tau1 <- 0.2
 
-# mix <- rbinom(p, 1, pi_left_mode)
-# beta <- numeric(p)
+mix <- rbinom(p, 1, pi_left_mode)
+beta <- numeric(p)
 
-# beta[mix == 0] <- rtruncnorm(
-#   sum(mix == 0),
-#   a = -3, b = 3,
-#   mean = -1.5,
-#   sd = tau0
-# )
+beta[mix == 0] <- rtruncnorm(
+  sum(mix == 0),
+  a = -3, b = 3,
+  mean = -1.5,
+  sd = tau0
+)
 
-# beta[mix == 1] <- rtruncnorm(
-#   sum(mix == 1),
-#   a = -3, b = 3,
-#   mean = 1.5,
-#   sd = tau1
-# )
+beta[mix == 1] <- rtruncnorm(
+  sum(mix == 1),
+  a = -3, b = 3,
+  mean = 1.5,
+  sd = tau1
+)
 
 
 # ---------------------------
